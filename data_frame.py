91f96f556620123da481d070d7d49e817cef49e8
GuitarSmasher21 <mic.sec@outlook.com>
2025-10-29 17:46:35 +0100

    Weitere Initialisierung gemacht bzgl. HanTa und weitere NLTK Verarbeitung

--- data_frame.py
+++ data_frame.py
@@ -1,8 +1,21 @@
-import pandas as pd
-import json
-import nltk
+## Python Module laden
+import pandas as pd ## DataFrame
+import json ## JSON Datei laden
+import nltk ## Tokenisierung
+import re ## Regular Expressions (Muster in Zeichenketten)
+from nltk.corpus import stopwords ## Liste für Stoppwörter aus NLTK
+from HanTa import HanoverTagger as ht ## Lemmartisierung über HanTa
 
+try: ## Versuch HanTa zu initialisieren, falls nicht Fallback auf WordNetLemmatizer
+    tagger_de = ht.HanoverTagger('morphmodel_ger.pgz') ## HanTa Modell muss geladen werden
+    print("HanTa Lemmatizer erfolgreich initialisiert mit Pfad.")
+except:
+    print("FEHLER: HanTa nicht gefunden oder falsche Initialisierung. Nutze Fallback-Lemmatizer.")
+    from nltk.stem import WordNetLemmatizer
+    lemmatizer = WordNetLemmatizer()
 
+## Python Script
+## 1. Schritt
 try:
     ## JSON-Datei einlesen
     with open('/Users/michi/Documents/GitHub/DLBDSEDA02_datenanalyse/unstructured_data.json', 'r', encoding='utf-8') as f:
@@ -18,9 +31,8 @@ try:
 except FileNotFoundError:
     print("FEHLER: 'daten.json' nicht gefunden. Bitte Dateipfad überprüfen.") ## Prüfung + Fehlermeldung auf falschem Dateipfad
 except Exception as e:
-    print(f"FEHLER beim Laden oder Parsen der Daten: {e}") ## Prüfung auf Prozessing-Fehler
-
-
+    print(f"FEHLER beim Laden oder Parsen der Daten: {e}") ## Prüfung auf Prozessing-Fehlecd s
+## 2. Schritt
 ## Spalte identifizieren, die die unstrukturierten Beschwerdetexte enthält.
 TEXT_COLUMN = 'sachverhalt' ##  Im Fall Berlin wird 'Sachverhalt' gewählt.
 
@@ -31,11 +43,9 @@ if TEXT_COLUMN in df.columns:
 else:
     print(f"\nFEHLER: Spalte '{TEXT_COLUMN}' nicht im DataFrame gefunden. Verfügbare Spalten: {df.columns.tolist()}") ## Ausgabe, sollte Sachverhalt nicht als Spalte gefunden werden
 
-'''
-# Lade die benötigten NLTK-Ressourcen, falls noch nicht geschehen
-nltk.download('punkt', quiet=True)
-nltk.download('stopwords', quiet=True)
-nltk.download('wordnet', quiet=True)
+try:
+    nltk.download('popular') ## Beliebte NLTK Datensätze installieren (basierend auf Dokumentation)
+except:
+    print("NLTK Ressourcen konnt nicht geladen werden.") ## Fehlermeldung für NLTK ausgeben
 
 # Ab hier haben Sie die zu analysierenden Texte in der Variable 'text_list'
-'''
